45d451bc78486614ac88a696d9f5575759076cdd
aigerimbakh <milashkalovec@gmail.com>
2022-11-26 11:22:25 +0300

    init

---
+++ src/main/java/com/example/llll/LlllApplication.java
@@ -0,0 +1,13 @@
+package com.example.llll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlllApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/llll/LlllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.llll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
